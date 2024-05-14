1c25c21be4bd66bbe6435f062d437461dfb85c74
OsvaldoPrajitno <osvaldoprajitno96@gmail.com>
2024-05-14 10:52:56 +0700

    Modified the down slope

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ let sprite, floor, sisyphus;
 let slope1, slope2;
 let canvas;
 let bar = 0;
+let initHeight;
 
 function preload() {
 	const startPosY = windowHeight * 0.64;
@@ -11,17 +12,17 @@ function preload() {
 	sprite.img = '/assets/Boulder.png'
 	sprite.d = 98;
 	sprite.bounciness = 0.2;
-	sprite.rotationDrag = 1;
-	sprite.mass = 4;
+	sprite.rotationDrag = 0.5;
+	sprite.mass = 6;
 	sprite.debug = false;
 
 	//ani = loadAni('/Sisyphus.png', {frameSize: [32, 32], frame: 8});
 
-	sisyphus = new Sprite(startPosX-35, startPosY + 25, 64, 64, 'd');
+	sisyphus = new Sprite(startPosX-35, startPosY, 64, 64, 'd');
 	
 	sisyphus.spriteSheet = '/assets/SisyphusNew.png'
 	sisyphus.anis.frameDelay = 4;
-	sisyphus.friction = 0.5;
+	sisyphus.friction = 0.2;
 	sisyphus.debug = false;
 	
 	sisyphus.addAnis({
@@ -34,33 +35,27 @@ function preload() {
 	floor.debug = false;
 	
 	//floor.addCollider(-floor.width * 1.628, floor.height * 0.812, [2000, -30],);
-	floor.addCollider(-10 -floor.w/2, 0, [[0, floor.h/2], [-floor.w/2 - (windowWidth * 0.5), 0], 
-	[windowWidth* 0.5, -floor.h], [floor.w/2, 0], [0, floor.h/2]]);
+	floor.addCollider(-floor.w*1.02, floor.h*0.17, [[0, floor.h], [-800, 0], 
+	[800, -floor.h]]);
 
 	floor.addCollider(floor.w, floor.h/4, [[0, -floor.h/2], [windowWidth/2, -floor.h], 
 	[0, floor.h*3], [-windowWidth/2, 0], [0, -floor.h*1.5]])
 
-	/*
-	floor.addCollider(350, -71, [[0, -floor.h/2], [floor.w/2, 0], [windowWidth*0.5, -floor.h/2], 
-	[0, floor.h * 3/2], [-windowWidth*0.5 - floor.w/2, 0], [0, -floor.h/2]])
-	/*
-	slope1 = new Sprite(floor.x - (floor.w/2), floor.y - (floor.h/2), 
-	[[-50, 40], [50, 0], [0, 40]]
-	)
-	slope1.collider = 's';
-	*/
+	
 }
 
 function setup() {
 	canvas = new Canvas(windowWidth, windowHeight);
-	world.gravity.y = 10;
+	world.gravity.y = 15;
+	initHeight = round(sisyphus.y)
 
 	console.log(windowWidth, windowHeight)
 }
 
 function draw() {
 	background('#b66d27');
-	text((windowHeight- 153 - round(sisyphus.y)), 50, 50);
+	
+	text(initHeight - round(sisyphus.y), 50, 50);
 
 	while (bar > -0.01) {
 		bar -= 0.0001
@@ -80,9 +75,9 @@ function draw() {
 	}
 
 	if (bar > 0) {
-		sisyphus.friction = 0.5;
-		sisyphus.bearing = -10;
-		sisyphus.vel.y = 0.3;
+		//sisyphus.friction = 0.2;
+		sisyphus.bearing = sisyphus.rotation;
+		//sisyphus.vel.y = 0.3;
 		sisyphus.applyForce(500);
 	}
 
